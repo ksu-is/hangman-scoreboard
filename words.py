f4ef90d9d877c9fd7ab7ca8eff13387d42a951d6
Carlos Alfaro <65995878+Nalley85@users.noreply.github.com>
2020-07-14 10:03:22 -0400

    Add files via upload

---
+++ words.py
@@ -0,0 +1,3 @@
+answerlist = [
+"chicken", "alpaca", "donkey", "horse", "llama", "sheep", "ferret", "woodpecker", "buffalo", "walrus", "vulture", "vicuna", "viper", "turkey", "tiger", "tortoise", "swordfish", "squirrel", "spider", "skunk", "shark"
+           ]
